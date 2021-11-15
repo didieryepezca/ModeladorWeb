45df4fc0feb66fd61386e404b09a70f2d31f1f8e
didieryepezca <didieryepezca@gmail.com>
2021-11-15 18:08:42 -0500

    Se creo la funcioanlidad de Registrar Usuario y Login con Usuario, además de las vistas principales: INICIO y ADMIN DE PROYECTOS, el botón de EDITAR y CREAR PROYECTO en la pantalla principal

--- ModeladorApp/Controllers/HomeController.cs
+++ ModeladorApp/Controllers/HomeController.cs
@@ -7,23 +7,37 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using ModeladorApp.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
 namespace ModeladorApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<ApplicationUser> userManager, ILogger<HomeController> logger)
         {
+            this.userManager = userManager;
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
+            var user = userManager.GetUserAsync(User);
+
+            ViewBag.usuarioId = user.Result.Id;
+
+
             return View();
         }
 
-        public IActionResult Privacy()
+
+        [Authorize]
+        public IActionResult AdminProyectos()
         {
             return View();
         }
