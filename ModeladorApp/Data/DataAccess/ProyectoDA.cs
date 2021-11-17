bf1f1c58fd5d359981fd34e97ff49625aa5ae1ff
didieryepezca <didieryepezca@gmail.com>
2021-11-17 13:16:19 -0500

    Continuamos con cambios

--- ModeladorApp/Data/DataAccess/ProyectoDA.cs
+++ ModeladorApp/Data/DataAccess/ProyectoDA.cs
@@ -18,11 +18,46 @@ namespace ModeladorApp.Data.DataAccess
 
             using (var db = new ApplicationDbContext())
             {
-                IQueryable<TB_PROYECTO> query = db.TB_PROYECTO.Include(c => c.TB_PERMISOS);
+                IQueryable<TB_PROYECTO> query = db.TB_PROYECTO.Include(c => c.TB_PERMISOS);               
+                
 
                 return query.ToList();
             }
         }
+
+        public object GetAllProyectosWithPermisos_2(string nombre, string usuario)
+        {
+            var ana = new List<TB_PROYECTO>();
+
+            using (var db = new ApplicationDbContext())
+            {                
+                    var query = (from proy in db.TB_PROYECTO
+                                 join permiso in db.TB_PERMISOS
+                                 on proy.ProyectoID equals permiso.ProyectoID
+
+                                 where proy.NombreProyecto == nombre 
+                                 && permiso.UsuarioCreacionId == usuario
+                                 && permiso.Permiso == "ADMIN"
+
+
+                                 select new TB_PROYECTO
+                                 {
+                                 ProyectoID = proy.ProyectoID,
+                                 NombreProyecto = proy.NombreProyecto,
+                                 DescripcionProyecto = proy.DescripcionProyecto,
+                                 FechaCreado = proy.FechaCreado,
+                                 FechaUltimaEdicion = proy.FechaUltimaEdicion,
+                                 PropietarioID = proy.PropietarioID,
+                                 PropietarioName = proy.PropietarioName,
+                                 
+                                 }).ToList();                   
+
+                return query;
+            }
+        }
+
+
+
 
         public IEnumerable<TB_PROYECTO> GetProyectosUsuario(string propiertarioId = "")
         {
